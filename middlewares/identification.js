b4ade1f5c4efee42bf36f746f81551648e091deb
facudelima <78700250+facudelima@users.noreply.github.com>
2026-05-07 17:46:20 -0300

    Refactor token retrieval logic in identifier middleware

--- middlewares/identification.js
+++ middlewares/identification.js
@@ -4,21 +4,21 @@ exports.identifier = (req, res, next) => {
 	let token;
 	if (req.headers.client === 'not-browser') {
 		token = req.headers.authorization;
-	} else {
+	} else { 
 		token = req.cookies['Authorization'];
-	}
+	} 
 
 	if (!token) {
 		return res.status(403).json({ success: false, message: 'Unauthorized' });
 	}
 
 	try {
-		const userToken = token.split(' ')[1];
+		const userToken = token.split(' ')[1]; 
 		const jwtVerified = jwt.verify(userToken, process.env.TOKEN_SECRET);
-		if (jwtVerified) {
-			req.user = jwtVerified;
+		if (jwtVerified) { 
+			req.user = jwtVerified; 
 			next();
-		} else {
+		} else { 
 			throw new Error('error in the token');
 		}
 	} catch (error) {
